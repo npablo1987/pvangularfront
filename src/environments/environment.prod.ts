f26e78dd7f5d0e205228144a211aac52b90564d8
Pablo Vilches Valenzuela <pvilches@pvilches.cl>
2025-07-24 08:50:26 -0400

    actulizaciones

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,13 @@ export const environment = {
     production: true,
     baseurlocal: "http://localhost:4200/",
     baseurl: "http://localhost:8000",
-
-  };
+    claveUnica: {
+        clientId:  'cd40ad229df147389504190c71de61f5',
+        clientSecret: '78ce7c028c9b4b629b7bc2b1b86fa199',
+        redirectUri: 'https://tusistema.gob.cl/claveunica/callback', // <= NO localhost
+        authUrl: 'https://accounts.claveunica.gob.cl/openid/authorize',
+        tokenUrl: 'https://accounts.claveunica.gob.cl/openid/token',
+        userInfoUrl: 'https://accounts.claveunica.gob.cl/openid/userinfo',
+        logoutUrl: 'https://accounts.claveunica.gob.cl/api/v1/accounts/app/logout'
+    },
+};
